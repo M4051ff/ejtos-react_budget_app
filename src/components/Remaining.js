18eeb8a2e50a4cb611756727163d57ebcbc80607
M4051ff <bussingtont@gmail.com>
2023-09-16 19:51:23 -0400

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,23 @@
+import { useContext } from "react";
+import {AppContext} from '../context/AppContext'
+
+const Remaining = () =>
+{
+    const {expenses} = useContext(AppContext);
+    const {budget} = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total,item) =>
+    {
+        return(total = total + item.cost);
+    }, 0);
+    
+   const remaining = (budget - totalExpenses);
+    
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+    return (
+        <div className = {`alert ${alertType}`}>
+            <span>Remaining: £{remaining}</span>
+        </div>
+    );
+};
+export default Remaining;
 
